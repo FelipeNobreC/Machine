ba9fca76e6a9f01304b93ede6285aacafbc1df9a
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2024-11-20 23:53:20 -0300

    fix: adjust comments

--- census/SVM.py
+++ census/SVM.py
@@ -6,9 +6,7 @@ import pickle
 
 class CensusClassifier:
     def __init__(self, model_path):
-        """
-        Inicializa o classificador com o caminho do modelo salvo e os dados.
-        """
+        # Inicializa o classificador com o caminho do modelo salvo e os dados.
         self.model_path = model_path
         self.X_train = None
         self.X_test = None
@@ -17,9 +15,7 @@ class CensusClassifier:
         self.svm = None
 
     def carregar_dados(self):
-        """
-        Carrega os dados processados salvos em um arquivo pickle.
-        """
+        # Carrega os dados processados salvos em um arquivo pickle.
         with open(self.model_path, 'rb') as f:
             self.X_train, self.X_test, self.y_train, self.y_test = pickle.load(f)
         
@@ -27,25 +23,19 @@ class CensusClassifier:
         self.y_train = self.y_train.ravel()
 
     def treinar_modelo(self, kernel='linear', random_state=1):
-        """
-        Treina o modelo SVM com os dados de treinamento.
-        """
+        # Treina o modelo SVM com os dados de treinamento.
         self.svm = SVC(kernel=kernel, random_state=random_state)
         self.svm.fit(self.X_train, self.y_train)
 
     def avaliar_modelo(self):
-        """
-        Avalia o modelo com os dados de teste.
-        """
+        # Avalia o modelo com os dados de teste.
         previsoes = self.svm.predict(self.X_test)
         print(previsoes)
         print(accuracy_score(self.y_test, previsoes))
         print(classification_report(self.y_test, previsoes))
 
     def consultar(self, *valores):
-        """
-        Faz previsões para novos dados fornecidos.
-        """
+        # Faz previsões para novos dados fornecidos.
         entrada = self.svm.fit(valores)
         previsao = self.svm.predict(entrada)
         print(accuracy_score(self.y_test, previsao))
